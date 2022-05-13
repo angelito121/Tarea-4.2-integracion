9d1b39dda472e385f4e905fedc5a57b98dde5a8c
Ángel Márquez Sánchez <53522218+angelito121@users.noreply.github.com>
2022-05-13 19:50:49 +0200

    Delete Clase2.java

--- src/Clase2.java
+++
@@ -1,8 +0,0 @@
-public class Clase2 {
-
-    public void main () {
-        System.out.println("He tenido que crear un nuevo repositorio desde el cual hacer los cambios y los fetch ya que me " +
-                "ne dejaba hacerlo desde el que lo hice con manuel ");
-
-    }
-}
