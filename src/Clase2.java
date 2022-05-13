2cba03ab2a70f5afdd1eb617f8e9915ebe26ee3e
Ángel Márquez Sánchez <angelmarqsanc@gmail.com>
2022-05-13 19:45:04 +0200

    Segundo commit desde la nueva plataforma

---
+++ src/Clase2.java
@@ -0,0 +1,8 @@
+public class Clase2 {
+
+    public void main () {
+        System.out.println("He tenido que crear un nuevo repositorio desde el cual hacer los cambios y los fetch ya que me " +
+                "ne dejaba hacerlo desde el que lo hice con manuel ");
+
+    }
+}
