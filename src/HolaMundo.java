adb8011480c9faced1b44d491558eaccae8427d6
Ángel Márquez Sánchez <angelmarqsanc@gmail.com>
2022-05-13 19:42:38 +0200

    Primer commit desde la nueva plataforma

---
+++ src/HolaMundo.java
@@ -0,0 +1,11 @@
+public class HolaMundo {
+
+        public void main () {
+            System.out.println("Hola mundo");
+            System.out.println("Primer cambio propio desde la nueva plataforma");
+
+        }
+
+
+    }
+
